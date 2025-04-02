f4b186ad8072fd9ced38103ad5ed861220690988
florian.wilms01 <florian.wilms01@stud.uni-goettingen.de>
2025-04-02 12:18:13 +0200

    clean pv script for data processing

--- scripts/pv.R
+++ scripts/pv.R
@@ -3,23 +3,7 @@
 library(tidyverse)
 library(janitor)
 
-plot_theme <- theme_bw() +
-  theme(
-    legend.position = "bottom",
-    legend.spacing.x = unit(0.7, 'cm'),
-    aspect.ratio = 0.5,
-    axis.text = element_text(size = 18),
-    axis.title.x = element_text(size = 18),
-    axis.title.y = element_text(size = 18),
-    legend.title = element_text(size = 15),
-    legend.text = element_text(size = 13),
-    legend.key.width = unit(1.5, "cm", data = NULL),
-    # panel.grid = element_line(
-    #   color = "grey",
-    #   size = 0.25,
-    #   linetype = 1
-    # )
-  )
+source("scripts/ggplot_themes.R")
 
 # load data ---------------------------------------------------------------
 
@@ -38,6 +22,7 @@ df_pv <- bind_rows(pv_2023, pv_2024) |>
 
 table(df_pv$sample_id)
 table(df_pv$campaign)
+
 # plot --------------------------------------------------------------------
 
 df_pv |> 
@@ -47,7 +32,7 @@ df_pv |>
   facet_wrap(year ~ sample_id, ncol = 4) +
   theme_bw()
 
-# analyse -----------------------------------------------------------------
+# analyze -----------------------------------------------------------------
 
 # 1. get missing end weights from max cumulative_mass ---------------------
 df_pv[is.na(df_pv$end_weight), ]
@@ -119,34 +104,73 @@ df_pv <- df_pv |>
 df_pv <- df_pv |> 
   left_join(pv_window, by = "curve_id")
 
-## 2.4 SWC and post-TLP models ---------------------------------------------
+## 2.4 SWC and post-TLP calculations ---------------------------------------------
 
-df_pv_mod <- df_pv |> 
-  group_by(year, campaign, sample_id) |> 
-  mutate(meas_no = 1:n(),
-         phase = case_when(meas_no < tlp_loc ~ "pre_tlp",
-                           meas_no >= tlp_loc ~ "post_tlp"),
-         leaf_water_g = initial_weight_g - mass_cum_g - dry_weight_g)
+df_pv_mod <- df_pv |>
+  group_by(year, campaign, sample_id) |>
+  mutate(
+    meas_no = 1:n(),
+    pre_tlp = case_when(meas_no <= tlp_loc ~ 1, meas_no > tlp_loc ~ 0),
+    post_tlp = case_when(meas_no < tlp_loc ~ 0, meas_no >= tlp_loc ~ 1),
+    leaf_water_g = initial_weight_g - mass_cum_g - dry_weight_g,
+    psi_MPa = - psi_bar / 10,
+    ## saturated water content = above and including TLP
+    swc_slope = sd(leaf_water_g[pre_tlp == 1]) / sd(psi_MPa[pre_tlp == 1]),
+    ## swc intercept = swc [g]
+    swc_intercept = mean(leaf_water_g[pre_tlp == 1]) - swc_slope * mean(psi_MPa[pre_tlp == 1]),
+    swc = swc_intercept / dry_weight_g,
+    ## relative water content
+    rwc_tot = leaf_water_g / swc_intercept,
+    rwc_tot_per = rwc_tot * 100,
+    rwc_tot_per_100 = 100 - rwc_tot_per,
+    ## tlp slope and intercept = below  and including TLP
+    tlp_slope = -sd(p_inv[post_tlp == 1]) / sd(rwc_tot_per_100[post_tlp == 1]) , 
+    tlp_intercept = mean(p_inv[post_tlp == 1]) - (tlp_slope * mean(rwc_tot_per_100[post_tlp == 1])), 
+    ## osmotic potential
+    psi_osmotic = -1 / (tlp_intercept + tlp_slope * rwc_tot_per_100), 
+    ## TLP: take osmotic potential at TLP (from visual estimation)
+    psi_tlp = case_when(meas_no == tlp_loc ~ psi_osmotic),
+    ## psi at full turgor
+    psi_ft = -(1 / tlp_intercept), 
+    ## turgor pressure
+    psi_turgor = psi_MPa - psi_osmotic,
+    ## apoplastic water fraction
+    apoplastic_water = 100 + (tlp_intercept / tlp_slope), 
+    ## symplastic water content = below TLP
+    rwc_sym = (rwc_tot - apoplastic_water / 100) / (1 - apoplastic_water / 100), 
+    rwc_sym_per = rwc_sym * 100, rwc_sym_per_100 = 100 - rwc_sym_per, ## osmotic potential
+    rwc_sym_per_100 = 100 - rwc_sym_per,
+    ## rwc at tlp
+    rwc_tot_tlp = case_when(meas_no == tlp_loc ~ rwc_tot_per),
+    rwc_sym_tlp = case_when(meas_no == tlp_loc ~ rwc_sym_per),
+    ## elasticity
+    elast_tot = sd(psi_turgor[pre_tlp == 1]) / sd(rwc_tot[pre_tlp == 1]),
+    elast_sym = sd(psi_turgor[pre_tlp == 1]) / sd(rwc_sym[pre_tlp == 1]),
+    ## capacitance
+    capacitance_ft_tot = sd(rwc_tot[pre_tlp == 1]) / sd(psi_MPa[pre_tlp == 1]),
+    capacitance_ft_sym = sd(rwc_sym[pre_tlp == 1]) / sd(psi_MPa[pre_tlp == 1]),
+    capacitance_tlp_tot = sd(rwc_tot[post_tlp == 1]) / sd(psi_MPa[post_tlp == 1]),
+    capacitance_tlp_sym = sd(rwc_sym[post_tlp == 1]) / sd(psi_MPa[post_tlp == 1])
+  ) |> 
+  ## fill partly empty columns
+  fill(swc_intercept:capacitance_tlp_sym, .direction = "downup")
 
 df_pv_pre_tlp <- df_pv_mod |>
-  filter(phase == "pre_tlp") |> 
+  filter(pre_tlp == "1") |> 
   group_by(year, campaign, sample_id) |> 
   mutate(
-    swc_slope = - sd(leaf_water_g) / sd(psi_bar),
+    swc_slope = -sd(leaf_water_g) / sd(psi_bar),
     swc_intercept = mean(leaf_water_g) - (swc_slope * mean(psi_bar)),
-    # swc_slope = coef(lm(leaf_water_g ~ psi_bar))[2],
-    # swc_intercept = coef(lm(leaf_water_g ~ psi_bar))[1],
     swc = swc_intercept / dry_weight_g,
-    # rwc = 100 * ((current_weight_g - dry_weight_g) / ((initial_weight_g) - dry_weight_g)),
     rwc_tot = leaf_water_g / swc_intercept,
     rwc_tot_per = rwc_tot * 100,
     rwc_tot_per_100 = 100 - rwc_tot_per)
 
 df_pv_mod <- df_pv_mod |> 
-  left_join(df_pv_pre_tlp |> select(year, campaign, sample_id, meas_no, swc_intercept), 
-            by = c("year", "campaign", "sample_id", "meas_no")) |> 
+  left_join(df_pv_pre_tlp |> dplyr::select(year, campaign, sample_id, meas_no, swc_intercept), 
+            by = c("year", "campaign", "sample_id", "meas_no", "swc_intercept")) |> 
   group_by(year, campaign, sample_id) |>
-  fill(swc_intercept, .direction = "downup") |>
+  fill("swc_intercept", .direction = "downup") |>
   mutate( 
     # rwc = 100 * ((current_weight_g - dry_weight_g) / ((initial_weight_g) - dry_weight_g)),
     rwc_tot = leaf_water_g / swc_intercept,
@@ -154,7 +178,7 @@ df_pv_mod <- df_pv_mod |>
     rwc_tot_per_100 = 100 - rwc_tot_per)
 
 df_pv_post_tlp <- df_pv_mod |> 
-  filter(phase == "post_tlp") |>
+  filter(post_tlp == "1") |>
   group_by(year, campaign, sample_id) |>
   mutate(
     tlp_slope = -sd(p_inv, na.rm = T) / sd(rwc_tot_per_100, na.rm = T),
@@ -192,18 +216,8 @@ write.csv(df_pv_mod, "data/PV/pv_data_calc.csv", row.names = F)
 
 ## 2.5 plot -----------------------------------------------------------------
 
-presentation_theme <- 
-  plot_theme +
-  theme(aspect.ratio = 1,
-        plot.background = element_rect(fill = "transparent"),
-        plot.title = element_text(size = 19),
-        plot.title.position = "plot",
-        plot.subtitle = element_text(size = 15, hjust = 0),
-        legend.background = element_rect(fill = "transparent"),
-        legend.key = element_rect(fill = "transparent"))
-
 df_pv_params <- read.csv("data/PV/pv_data_calc.csv")
-df_pv_params_summ <- read.csv("data/PV/pv_data_calc_summ.csv")
+df_pv_params_summ <- read.csv("data/calculated_parameters/df_pv_params_summ.csv")
 
 df_pv_params <- df_pv_params |> 
   mutate(species = recode(species, "FASY" = "beech", "FREX" = "ash")) 
@@ -221,167 +235,6 @@ df_pv_params %>%
        y =  "Psi Inverse") +
   facet_wrap(~curve_id, scales = "free") +
   theme_bw()
-
-### tlp ####
-plot_psi_tlp <- df_pv_params |> 
-  group_by(year, campaign, species) |>
-  mutate(date = date |> as.character() |> 
-           str_replace_all("2023|2024", "2000") |> as.Date("%Y-%m-%d"),
-         year = as.factor(year)) |>
-  summarise(date = unique(date),
-            mean_psi_tlp = mean(psi_tlp, na.rm = T),
-            sd_psi_tlp = sd(psi_tlp, na.rm = T)) |> 
-  ungroup() |> 
-  ggplot(aes(col = species, lty = year)) +
-  geom_point(aes(x = date, y = mean_psi_tlp, 
-                 col = species, lty = year, group = interaction(species,campaign)),
-             size = 2, position = position_dodge(0.5)) +
-  geom_errorbar(
-    aes(x = date, 
-        ymin = mean_psi_tlp - sd_psi_tlp, ymax = mean_psi_tlp + sd_psi_tlp,
-        ), size = 0.5, position = position_dodge(0.5),
-    width = 0.2) +
-  geom_line(aes(x = date, y = mean_psi_tlp), size = 0.5) +
-  scale_color_discrete("Year") +
-  labs(y = "Turgor loss point [MPa]",
-       x = "Date",
-       title = "Water potentials at Turgor Loss Point [MPa]",
-       subtitle = "mean and standard deviation, n = 4 samples per species") +
-  guides(col = guide_legend("Species", ncol = 1),
-         linetype = guide_legend("Year", ncol = 1)) +
-  scale_color_viridis_d(option = "inferno", begin = 0.2, end = 0.8) +
-  presentation_theme
-
-plot_psi_tlp
-ggsave("figures/pv_psi_tlp.png", plot_psi_tlp, width = 6, height = 6)
-
-### elasticity ----
-#### symplastic ----
-plot_sym_elasticity <- df_pv_params |> 
-  group_by(year, campaign, species) |>
-  mutate(date = date |> as.character() |> 
-           str_replace_all("2023|2024", "2000") |> as.Date("%Y-%m-%d"),
-         year = as.factor(year)) |>
-  summarise(date = unique(date),
-            mean_elasticity = mean(elasticity_sym, na.rm = T),
-            sd_elasticity = sd(elasticity_sym, na.rm = T)) |> 
-  ggplot() +
-  geom_point(aes(x = date, y = mean_elasticity, 
-                 col = species, lty = year, group = interaction(species,campaign)),
-             size = 2, position = position_dodge(0.5)) +
-  geom_errorbar(
-    aes(x = date, 
-        ymin = mean_elasticity - sd_elasticity, ymax = mean_elasticity + sd_elasticity,
-        col = species, lty = year ), size = 0.5, position = position_dodge(0.5),
-    width = 0.2) +
-  geom_line(aes(x = date, y = mean_elasticity, col = species, lty = year), size = 0.5) +
-  scale_color_discrete("Year") +
-  labs(y = "Elasticity [MPa]", 
-       title = "Bulk modulus of symplastic elasticity [MPa]",
-       subtitle = "mean and standard deviation, n = 4 samples per species") +
-  guides(col = guide_legend("Species", ncol = 1),
-         linetype = guide_legend("Year", ncol = 1)) +
-  scale_color_viridis_d(option = "inferno", begin = 0.2, end = 0.8) +
-  presentation_theme
-
-plot_sym_elasticity
-ggsave("figures/plot_sym_elasticity.png", plot_sym_elasticity, width = 6, height = 6)
-
-#### total ----
-
-plot_total_elasticity <- df_pv_params |> 
-  group_by(year, campaign, species) |>
-  mutate(date = date |> as.character() |> 
-           str_replace_all("2023|2024", "2000") |> as.Date("%Y-%m-%d"),
-         year = as.factor(year)) |>
-  summarise(date = unique(date),
-            mean_elasticity = mean(elasticity_tot, na.rm = T),
-            sd_elasticity = sd(elasticity_tot, na.rm = T)) |> 
-  ggplot() +
-  geom_point(aes(x = date, y = mean_elasticity, 
-                 col = species, lty = year, group = interaction(species,campaign)),
-             size = 2, position = position_dodge(0.5)) +
-  geom_errorbar(
-    aes(x = date, 
-        ymin = mean_elasticity - sd_elasticity, ymax = mean_elasticity + sd_elasticity,
-        col = species, lty = year ), size = 0.5, position = position_dodge(0.5),
-    width = 0.2) +
-  geom_line(aes(x = date, y = mean_elasticity, col = species, lty = year), size = 0.5) +
-  scale_color_discrete("Year") +
-  labs(y = "Elasticity",
-       title = "Bulk modulus of total elasticity [MPa]",
-       subtitle = "mean and standard deviation, n = 4 samples per species") +
-  scale_color_viridis_d(option = "inferno", begin = 0.2, end = 0.8) +
-  presentation_theme
-  
-plot_total_elasticity
-ggsave("figures/plot_total_elasticity.png", plot_total_elasticity, width = 6, height = 6)
-
-### capacitance ----
-plot_capacitance_tlp <- df_pv_params |> 
-  group_by(year, campaign, species) |>
-  mutate(date = date |> as.character() |> 
-           str_replace_all("2023|2024", "2000") |> as.Date("%Y-%m-%d"),
-         year = as.factor(year)) |>
-  summarise(date = unique(date),
-            mean_cap = mean(capacitance_tlp_sym, na.rm = T),
-            sd_cap = sd(capacitance_tlp_sym, na.rm = T)) |> 
-  ggplot(aes(col = species, lty = year)) +
-  geom_point(aes(x = date, y = mean_cap, 
-                 group = interaction(species,campaign)),
-             size = 2, position = position_dodge(0.5)) +
-  geom_errorbar(
-    aes(x = date, 
-        ymin = mean_cap - sd_cap, ymax = mean_cap + sd_cap), 
-    size = 0.5, position = position_dodge(0.5),
-    width = 0.2) +
-  geom_line(aes(x = date, y = mean_cap), size = 0.5) +
-  scale_color_viridis_d(option = "magma", begin = 0.2, end = 0.8) +
-  labs(y = "Capacitance",
-       x = "Date",
-       title = "Symplastic capacitance at Turgor Loss Point",
-       subtitle = "mean and standard deviation, n = 4 samples per species") +
-  guides(col = guide_legend("Species", ncol = 1),
-         linetype = guide_legend("Year", ncol = 1)) +
-  scale_color_viridis_d(option = "magma", begin = 0.2, end = 0.8) +
-  presentation_theme
-
-plot_capacitance_tlp
-ggsave("figures/plot_capacitance_tlp.png", plot_capacitance_tlp, width = 6, height = 6)
-
-### RWC at tlp ----
-
-plot_rwc_tlp <- df_pv_params |> 
-  group_by(year, campaign, species) |>
-  mutate(date = date |> as.character() |> 
-           str_replace_all("2023|2024", "2000") |> as.Date("%Y-%m-%d"),
-         year = as.factor(year)) |>
-  summarise(date = unique(date),
-            mean_rwc = mean(rwc_tot_tlp, na.rm = T),
-            sd_rwc = sd(rwc_tot_tlp, na.rm = T)) |> 
-  ggplot(aes(col = species, lty = year)) +
-  geom_point(aes(x = date, y = mean_rwc, 
-                 group = interaction(species,campaign)),
-             size = 2, position = position_dodge(0.5)) +
-  geom_errorbar(
-    aes(x = date, 
-        ymin = mean_rwc - sd_rwc, ymax = mean_rwc + sd_rwc), 
-    size = 0.5, position = position_dodge(0.5),
-    width = 0.2) +
-  geom_line(aes(x = date, y = mean_rwc), size = 0.5) +
-  ylim(70, 100) +
-  scale_color_viridis_d(option = "magma", begin = 0.2, end = 0.8) +
-  labs(y = "RWC at TLP [%]",
-       x = "Date",
-       title = "Relative water content at Turgor Loss Point",
-       subtitle = "mean and standard deviation, n = 4 samples per species") +
-  guides(col = guide_legend("Species", ncol = 1),
-         linetype = guide_legend("Year", ncol = 1)) +
-  scale_color_viridis_d(option = "magma", begin = 0.2, end = 0.8) +
-  presentation_theme
-
-plot_rwc_tlp
-ggsave("figures/plot_rwc_tlp.png", plot_rwc_tlp, width = 6, height = 6)
 
 ## check -------------------------------------------------------------------
 
@@ -407,102 +260,8 @@ df_pv_params_summ <- df_pv_params_summ |>
   mutate(date = date |> str_replace("2023-07-19", "2023-07-17"),
          date = date |> str_replace("2023-08-11", "2023-08-09"))
 
-write.csv(df_pv_params_summ, "data/PV/pv_data_calc_summ.csv", row.names = F) 
+write.csv(df_pv_params_summ, "data/calculated_parameters/df_pv_params_summ.csv", row.names = F) 
 
-# simple analysis ---------------------------------------------------------
-
-
-## 2. get lm per sample/campaign/year from the last 5 values p_inv ~ rwc_tot
-
-df_pv_nest <- df_pv |> 
-  nest(data = -c(year, campaign, sample_id)) |>
-  mutate(data_tlp = map(data, ~ tail(.x, 5)),
-         model = map(data_tlp, ~ lm(p_inv ~ rwc_tot, data = .x)))
-
-## 3. get intercept and transform into tlp
-df_pv_nest <- df_pv_nest |> 
-  mutate(intercept = map(model, ~ coef(.x)[1]) |> unlist() |> as.numeric(),
-         slope = map(model, ~ coef(.x)[2]) |> unlist() |> as.numeric(),
-         psi_ft_bar = map(intercept, ~ 1/.x*10) |> unlist() |> as.numeric(), ## transform intercept for psi at full turgor
-         tlp = map(data_tlp, ~ .x |> slice_min(rwc_tot, n = 1) |> as.data.frame() |> pull(rwc_tot)) ,
-         psi_tlp_inv = map2(model, tlp, ~ predict(.x, newdata = list("rwc_tot" = .y)) |> as.numeric()) |> unlist() |> as.numeric(),
-         psi_tlp_bar = map(psi_tlp_inv, ~ 1/.x*10) |> as.numeric()
-  ) 
-
-df_pv_nest$psi_tlp_bar
-
-ggplot(df_pv |> filter(year == 2023, sample_id == "FASY_01")) +
-  geom_point(aes(x = rwc_tot, y = p_inv, group = interaction(sample_id, year),
-                 col = as.factor(sample_id))) +
-  scale_color_discrete("Sample") +
-  geom_abline(aes(intercept = intercept, slope = slope), 
-              data = df_pv_nest |> filter(year == 2023, sample_id == "FASY_01"), col = "red") +
-  ylab("Inverse water potential") +
-  xlab("Relative water content [%]") +
-  ggtitle("PV curves for FASY_01 for four campaigns 2023") +
-  facet_wrap(campaign~species, ncol = 4) +
-  theme_bw() +
-  theme(legend.position = "bottom")
-
-
-ggplot(df_pv |> filter(year == 2024, sample_id == "FASY_01")) +
-  geom_point(aes(x = rwc_tot, y = p_inv, group = interaction(sample_id, year),
-                 col = as.factor(sample_id))) +
-  scale_color_discrete("Sample") +
-  geom_abline(aes(intercept = intercept, slope = slope), 
-              data = df_pv_nest |> filter(year == 2024, sample_id == "FASY_01"), col = "red") +
-  ylab("Inverse water potential") +
-  xlab("Relative water content [%]") +
-  ggtitle("PV curves for FASY_01 for four campaigns 2024") +
-  facet_wrap(campaign~species, ncol = 4) +
-  theme_bw() +
-  theme(legend.position = "bottom")
-## summary -----------------------------------------------------------------
-
-df_pv_nest$psi_tlp_bar
-
-df_pv_tlp_summ <- df_pv_nest |> 
-  unnest(data) |> 
-  group_by(year, date, species) |> 
-  summarise(mean_tlp_MPa = - mean(psi_tlp_bar) / 10,
-            sd_tlp_MPa = - sd(psi_tlp_bar) / 10,
-            mean_ft_MPa = - mean(psi_ft_bar) / 10,
-            sd_ft_MPa = - sd(psi_ft_bar) / 10)
-
-df_pv_tlp_summ$date = df_pv_tlp_summ$date |> str_replace_all("2023|2024", "2000") |> 
-  as.Date("%Y-%m-%d")
-
-df_pv_tlp_summ |> 
-  ggplot() +
-  geom_point(aes(x = date, y = mean_tlp_MPa, col = as.factor(year)), position = position_dodge(width = 0.2), size = 2) +
-  geom_errorbar(
-    aes(x = date, 
-        ymin = mean_tlp_MPa - sd_tlp_MPa, 
-        ymax = mean_tlp_MPa + sd_tlp_MPa,
-        col = as.factor(year)),
-    width = 0.2, position = position_dodge(), size = 1) +
-  scale_color_discrete("Year") +
-  ylab("Turgor loss point [MPa]") +
-  ggtitle("Water potentials at Turgor Loss Point [MPa]",
-          subtitle = "mean and standard deviation, n = 4 samples per species") + 
-  facet_wrap(~species, ncol = 2) +
-  plot_theme +
-  theme(aspect.ratio = 1)
-
-df_pv_tlp_summ |> 
-  ggplot() +
-  geom_point(aes(x = campaign, y = mean_ft_MPa, col = as.factor(year))) +
-  geom_errorbar(
-    aes(x = campaign, 
-        ymin = mean_ft_MPa - sd_ft_MPa, 
-        ymax = mean_ft_MPa + sd_ft_MPa,
-        col = as.factor(year)),
-    width = 0.2) +
-  scale_color_discrete("Year") +
-  ylab("Water potential at full turgor [MPa]") +
-  ggtitle("Water potential at full turgor") +
-  facet_wrap(~species, ncol = 2) +
-  plot_theme
 
 
 
@@ -624,7 +383,7 @@ ggplot() +
 # clean data --------------------------------------------------------------
 library(readxl)
 
-# 2023 --------------------------------------------------------------------
+## 2023 --------------------------------------------------------------------
 
 pv_2023 <- read_csv("data/PV/2023/pv_data_clean.csv") |> 
   clean_names() |> 
@@ -652,7 +411,7 @@ pv_2023$sample_id <- pv_2023$sample_id |>
 
 write.csv(pv_2023, "data/PV/pv_2023.csv", row.names = F)
 
-# 2024 --------------------------------------------------------------------
+## 2024 --------------------------------------------------------------------
 
 pv_2024_files <- list.files("data/PV/2024", full.names = T, recursive = F, pattern = "xlsx")
 pv_2024_sheets <- map(pv_2024_files, excel_sheets)
